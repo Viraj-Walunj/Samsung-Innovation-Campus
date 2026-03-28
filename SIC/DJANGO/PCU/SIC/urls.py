3e6423b856326cf113c675b7b356b11cc4b66e3c
Viraj-Walunj <virajwalunj96@gmmail.com>
2026-03-28 17:08:22 +0530

    Moved project into SIC_Project folder

--- SIC/DJANGO/PCU/SIC/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .import views
-
-urlpatterns = [
-    path("abc",views.hellow_word),
-    path("xyz",views.hello)
-]
--- SIC/DJANGO/PCU/SIC/views.py
+++
@@ -1,7 +0,0 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
-def hellow_word(request):
-    return HttpResponse("Hello Pune")
-def hello(request):
-    return HttpResponse("Hello Mumbai")
-# Create your views here.
